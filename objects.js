77951a16476ae44f2f4ed9fae36a3dc4b8fd6c1d
David-MJ-Guest <davemjguest@gmail.com>
2023-09-04 10:30:42 +0100

    objects

---
+++ objects.js
@@ -0,0 +1,30 @@
+var darthVader = {
+    allegiance: "Empire",
+    weapon: "lightsaber",
+    sith: true
+};
+
+console.log("Darth Vader's allegiance:", darthVader.allegiance);
+console.log("Darth Vader's weapon:", darthVader.weapon);
+console.log("If Darth Vader is a Sith:", darthVader.sith );
+console.log("The value of Jedi from Darth Vader:", darthVader.jedi);
+console.log("Number of properties Darth Vader has:", Obkect.keys(darthVader).length);
+
+darthVader.children = 2;
+darthVader.childNames = ['Luke', 'Leia'];
+
+console.log("Darth Vader's children:", darthVader.children);
+comsole.log("First child's name:", darthVader.childNames[0]);
+
+for (var key in darthVader) {
+    if (darthVader.hasOwnProperty(key)) {
+    console.log("Key:", key, "Value", darthVader[key]);
+}
+}
+
+darthVader.allegiance = "The light side";
+console.log("\nDarth Vader after changing allegiance:", darthVader);
+
+delete darthVader.children;
+console.log("\nDarth Vader after deleting children:", darthVader);
+
